377d58c975e470ed62b919428a1aef2e5e350969
Jordi Garcia Castillon <gcjordi@hotmail.com>
2026-09-10 14:46:44 +0200

    Fix CodeQL unused import note

--- RSA_module.py
+++ RSA_module.py
@@ -4,10 +4,18 @@ The original 2021 RSA_module.py was intentionally replaced. Use
 quantum_crypto_lab.rsa_demo for the safe, clearly labelled educational API.
 """
 
-from quantum_crypto_lab.rsa_demo import (  # noqa: F401
-    ToyRSAKeyPair,
-    build_toy_keypair,
-    decrypt_int,
-    encrypt_int,
-    recover_private_key_from_factors,
-)
+from quantum_crypto_lab import rsa_demo as _rsa_demo
+
+ToyRSAKeyPair = _rsa_demo.ToyRSAKeyPair
+build_toy_keypair = _rsa_demo.build_toy_keypair
+decrypt_int = _rsa_demo.decrypt_int
+encrypt_int = _rsa_demo.encrypt_int
+recover_private_key_from_factors = _rsa_demo.recover_private_key_from_factors
+
+__all__ = [
+    "ToyRSAKeyPair",
+    "build_toy_keypair",
+    "decrypt_int",
+    "encrypt_int",
+    "recover_private_key_from_factors",
+]
